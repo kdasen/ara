edcfdeb2da568777845566294f33bf4a129123d2
Kilian Dasen <dasenk@student.ethz.ch>
2022-06-30 17:32:59 +0200

    📊 set N=16

--- apps/ara-benchmark/main.c
+++ apps/ara-benchmark/main.c
@@ -7,7 +7,7 @@
 #include "runtime.h"
 
 
-#define N 4
+#define N 16
 
 int64_t A[N] __attribute__((aligned(32 * NR_LANES), section(".l2")));
 int64_t B[N] __attribute__((aligned(32 * NR_LANES), section(".l2")));
